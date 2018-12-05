87f16daed6eebede3bde74d11aab4bf81fc0ca0d
mrdulin <novaline.dulin@gmail.com>
2018-12-05 16:15:40 +0800

    improve insert-data-in-multiple-tables sample

--- src/insert-data-in-multiple-tables/index.spec.ts
+++ src/insert-data-in-multiple-tables/index.spec.ts
@@ -1,12 +1,13 @@
 import faker from 'faker';
 
 import { knex } from '../db-local';
-import { createUserRaw, createUser, createUserWithAddress } from './';
+import { createUserRaw, createUser, createUserWithAddress, createUserTransaction } from './';
 
 afterAll(async () => {
   await knex.destroy();
 });
 
+// TODO:
 describe.skip('createUserRaw', () => {
   it('should create user correctly without address', async () => {
     const user = { user_id: 4, user_name: faker.name.findName() };
@@ -16,7 +17,7 @@ describe.skip('createUserRaw', () => {
   });
 });
 
-describe('createUser', () => {
+describe.skip('createUser', () => {
   afterEach(async () => {
     await knex('users')
       .where({ user_id: 5 })
@@ -32,7 +33,7 @@ describe('createUser', () => {
   });
 });
 
-describe('createUserWithAddress', () => {
+describe.skip('createUserWithAddress', () => {
   afterEach(async () => {
     await knex('users')
       .where({ user_id: 5 })
@@ -55,7 +56,7 @@ describe('createUserWithAddress', () => {
     });
   });
 
-  it('should insert user, do not insert address when there is an address has the same address_id with insert one', async () => {
+  it('should insert user, not insert address when there is an address has the same address_id with insert one', async () => {
     const user = {
       user_id: 3,
       user_name: faker.name.findName(),
@@ -79,5 +80,49 @@ describe('createUserWithAddress', () => {
 
     expect(userCount).toBe(0);
     expect(addressInserted).toEqual(user.address);
+  });
+});
+
+describe('createUserTransaction', () => {
+  afterEach(async () => {
+    await knex('users')
+      .where({ user_id: 4 })
+      .del();
+    await knex('addresses')
+      .where({ address_id: 2 })
+      .del();
+  });
+  it('should not insert user and address when there is an address has the same address_id with insert one', async () => {
+    const user = {
+      user_id: 3,
+      user_name: faker.name.findName(),
+      address: {
+        address_id: 2,
+        address_name: faker.address.city()
+      }
+    };
+
+    await expect(createUserTransaction(user)).rejects.toThrow(
+      /duplicate key value violates unique constraint "users_pkey"/
+    );
+  });
+
+  it('should insert user and address correctly', async () => {
+    const user = {
+      user_id: 4,
+      user_name: faker.name.findName(),
+      address: {
+        address_id: 2,
+        address_name: faker.address.city()
+      }
+    };
+
+    const actualValue = await createUserTransaction(user);
+    console.log(actualValue);
+    expect(actualValue).toEqual({
+      user_id: user.user_id,
+      user_name: user.user_name,
+      address_id: user.address.address_id
+    });
   });
 });
--- src/insert-data-in-multiple-tables/index.ts
+++ src/insert-data-in-multiple-tables/index.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import Knex from 'knex';
 
 import { knex } from '../db-local';
 
@@ -42,6 +43,42 @@ async function createUser(data) {
     .get('0');
 }
 
-async function createUserTransaction() {}
+async function createUserTransaction(data) {
+  const user = _.omit(data, ['address']);
+  return knex
+    .transaction((trx: Knex.Transaction) => {
+      knex('addresses')
+        .transacting(trx)
+        .insert(data.address, 'address_id')
+        .get('0')
+        .then(addressId => {
+          console.log('insert address success. addressId: ', addressId, typeof addressId);
+          user.address_id = addressId;
+          return knex('users')
+            .transacting(trx)
+            .insert(user)
+            .returning('*');
+        })
+        .get('0')
+        .then(val => {
+          console.log('val: ', val);
+          console.log('transaction commit');
+          trx.commit(val);
+        })
+        .catch(err => {
+          console.log('transaction rollback');
+          trx.rollback(err);
+        });
+    })
+    .then(resp => {
+      console.log('Transaction complete.');
+      return resp;
+    })
+    .catch(err => {
+      // console.error(err);
+      console.log('Transaction failed.');
+      return Promise.reject(err);
+    });
+}
 
 export { createUserRaw, createUser, createUserTransaction, createUserWithAddress };
