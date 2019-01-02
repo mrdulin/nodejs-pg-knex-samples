02497907cc5a850b12c90a4a73c81ab7ead4be7c
mrdulin <novaline.dulin@gmail.com>
2019-01-02 19:24:14 +0800

    add cloud-sql sample

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,4 +1,11 @@
+import dotenv from 'dotenv';
+import path from 'path';
 import { ISettings } from './@types';
+
+const result = dotenv.config({ path: path.resolve(__dirname, '../.env') });
+if (result.error) {
+  throw result.error;
+}
 
 const settings: ISettings = {
   development: {
@@ -9,6 +16,27 @@ const settings: ISettings = {
       database: 'nodejs-pg-knex-samples',
       user: 'sampleadmin',
       password: 'samplepass'
+    },
+    debug: true,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: process.env.SQL_DATABASE,
+      user: process.env.SQL_USER,
+      password: process.env.SQL_PASSWORD,
+      socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
     },
     debug: true,
     pool: {
