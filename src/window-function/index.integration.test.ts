3eaf3ba997b4b2d9f4cec387933fe1d6608c6195
mrdulin <novaline.dulin@gmail.com>
2020-01-03 14:56:41 +0800

    add window function sample

--- src/window-function/index.integration.test.ts
+++ src/window-function/index.integration.test.ts
@@ -6,7 +6,7 @@ describe('window-function', () => {
     knex.destroy(done);
   });
 
-  const expectedDataset = [
+  const expectedAvgScoreDataset = [
     { id: 1, subject: 'Chinese', stu_name: 'Cortez Nader', score: 70, avgscore: 73.3333333333333 },
     { id: 2, subject: 'Chinese', stu_name: 'Westley Steuber IV', score: 70, avgscore: 73.3333333333333 },
     { id: 3, subject: 'Chinese', stu_name: 'Camilla Reilly', score: 80, avgscore: 73.3333333333333 },
@@ -20,19 +20,92 @@ describe('window-function', () => {
 
   describe('#avgscoreUsingJoin', () => {
     it('should pass', async () => {
-      expect.assertions(2);
       const rows = await queries.avgscoreUsingJoin(knex);
       expect(rows).toHaveLength(9);
-      expect(rows).toEqual(expectedDataset);
+      expect(rows).toEqual(expectedAvgScoreDataset);
     });
   });
 
   describe('#avgscoreUsingWindowFunction', () => {
     it('should pass', async () => {
-      expect.assertions(2);
       const rows = await queries.avgscoreUsingWindowFunction(knex);
       expect(rows).toHaveLength(9);
-      expect(rows).toEqual(expectedDataset);
+    });
+  });
+
+  describe('#rowNumber', () => {
+    const expectedRowNumberDataset = [
+      { row_number: '1', id: 2, subject: 'Chinese', stu_name: 'Westley Steuber IV', score: 70 },
+      { row_number: '2', id: 1, subject: 'Chinese', stu_name: 'Cortez Nader', score: 70 },
+      { row_number: '3', id: 3, subject: 'Chinese', stu_name: 'Camilla Reilly', score: 80 },
+      { row_number: '1', id: 6, subject: 'English', stu_name: 'Marta Homenick', score: 60 },
+      { row_number: '2', id: 4, subject: 'English', stu_name: 'Dortha Abbott DDS', score: 75 },
+      { row_number: '3', id: 5, subject: 'English', stu_name: 'Claudia Mante', score: 90 },
+      { row_number: '1', id: 9, subject: 'Math', stu_name: 'Margret Emmerich DDS', score: 65 },
+      { row_number: '2', id: 7, subject: 'Math', stu_name: 'Floyd Auer', score: 80 },
+      { row_number: '3', id: 8, subject: 'Math', stu_name: 'Devyn Kessler', score: 99 }
+    ];
+
+    it('should pass', async () => {
+      const rows = await queries.rowNumber(knex);
+      expect(rows).toHaveLength(9);
+      expect(rows).toEqual(expectedRowNumberDataset);
+    });
+  });
+
+  describe('#rank', () => {
+    const expectRankDataset = [
+      { rank: '1', id: 2, subject: 'Chinese', stu_name: 'Westley Steuber IV', score: 70 },
+      { rank: '1', id: 1, subject: 'Chinese', stu_name: 'Cortez Nader', score: 70 },
+      { rank: '3', id: 3, subject: 'Chinese', stu_name: 'Camilla Reilly', score: 80 },
+      { rank: '1', id: 6, subject: 'English', stu_name: 'Marta Homenick', score: 60 },
+      { rank: '2', id: 4, subject: 'English', stu_name: 'Dortha Abbott DDS', score: 75 },
+      { rank: '3', id: 5, subject: 'English', stu_name: 'Claudia Mante', score: 90 },
+      { rank: '1', id: 9, subject: 'Math', stu_name: 'Margret Emmerich DDS', score: 65 },
+      { rank: '2', id: 7, subject: 'Math', stu_name: 'Floyd Auer', score: 80 },
+      { rank: '3', id: 8, subject: 'Math', stu_name: 'Devyn Kessler', score: 99 }
+    ];
+    it('should pass', async () => {
+      const rows = await queries.rank(knex);
+      expect(rows).toHaveLength(9);
+      expect(rows).toEqual(expectRankDataset);
+    });
+  });
+
+  describe('#firstValue', () => {
+    const expectedFirstValueDataset = [
+      { first_value: 70, id: 2, subject: 'Chinese', stu_name: 'Westley Steuber IV', score: 70 },
+      { first_value: 70, id: 1, subject: 'Chinese', stu_name: 'Cortez Nader', score: 70 },
+      { first_value: 70, id: 3, subject: 'Chinese', stu_name: 'Camilla Reilly', score: 80 },
+      { first_value: 60, id: 6, subject: 'English', stu_name: 'Marta Homenick', score: 60 },
+      { first_value: 60, id: 4, subject: 'English', stu_name: 'Dortha Abbott DDS', score: 75 },
+      { first_value: 60, id: 5, subject: 'English', stu_name: 'Claudia Mante', score: 90 },
+      { first_value: 65, id: 9, subject: 'Math', stu_name: 'Margret Emmerich DDS', score: 65 },
+      { first_value: 65, id: 7, subject: 'Math', stu_name: 'Floyd Auer', score: 80 },
+      { first_value: 65, id: 8, subject: 'Math', stu_name: 'Devyn Kessler', score: 99 }
+    ];
+    it('should pass', async () => {
+      const rows = await queries.firstValue(knex);
+      expect(rows).toHaveLength(9);
+      expect(rows).toEqual(expectedFirstValueDataset);
+    });
+  });
+
+  describe('#windowAlias', () => {
+    it('should pass', async () => {
+      const rows = await queries.windowAlias(knex);
+      expect(rows).toHaveLength(9);
+      expect(rows).toEqual([
+        { avg: 73.3333333333333, sum: 220, id: 1, subject: 'Chinese', stu_name: 'Cortez Nader', score: 70 },
+        { avg: 73.3333333333333, sum: 220, id: 2, subject: 'Chinese', stu_name: 'Westley Steuber IV', score: 70 },
+        { avg: 73.3333333333333, sum: 220, id: 3, subject: 'Chinese', stu_name: 'Camilla Reilly', score: 80 },
+        { avg: 75, sum: 225, id: 4, subject: 'English', stu_name: 'Dortha Abbott DDS', score: 75 },
+        { avg: 75, sum: 225, id: 5, subject: 'English', stu_name: 'Claudia Mante', score: 90 },
+        { avg: 75, sum: 225, id: 6, subject: 'English', stu_name: 'Marta Homenick', score: 60 },
+        { avg: 81.3333333333333, sum: 244, id: 7, subject: 'Math', stu_name: 'Floyd Auer', score: 80 },
+        { avg: 81.3333333333333, sum: 244, id: 8, subject: 'Math', stu_name: 'Devyn Kessler', score: 99 },
+        { avg: 81.3333333333333, sum: 244, id: 9, subject: 'Math', stu_name: 'Margret Emmerich DDS', score: 65 }
+      ]);
     });
   });
 });
--- src/window-function/index.ts
+++ src/window-function/index.ts
@@ -19,3 +19,36 @@ export async function avgscoreUsingWindowFunction(knex: Knex) {
   const res = await knex.raw(query);
   return res.rows;
 }
+
+export async function rowNumber(knex: Knex) {
+  const query = `
+    select row_number() over(partition by subject order by score), * from score;
+  `;
+  const res = await knex.raw(query);
+  return res.rows;
+}
+
+export async function rank(knex: Knex) {
+  const query = `
+    select rank() over(partition by subject order by score), * from score;
+  `;
+  const res = await knex.raw(query);
+  return res.rows;
+}
+
+export async function firstValue(knex: Knex) {
+  const query = `
+    select first_value(score) over(partition by subject order by score), * from score;
+  `;
+  const res = await knex.raw(query);
+  return res.rows;
+}
+
+// 窗口函数别名
+export async function windowAlias(knex: Knex) {
+  const query = `
+    select avg(score) over(r), sum(score) over(r), * from score window r as (partition by subject);
+  `;
+  const res = await knex.raw(query);
+  return res.rows;
+}
